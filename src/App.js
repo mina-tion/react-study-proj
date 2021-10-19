e4acbc22ed7be17a155306d44bb3b8bfe42d769c
Anastasia Skripnik <scripnik.a21@cf.dnu.dp.ua>
2021-10-20 01:15:07 +0300

    add user events

--- src/App.js
+++ src/App.js
@@ -1,57 +1,13 @@
 import React from "react"
 
-// const App = (props) => {
 
-//   const [counter, setCounter] = useState(0)
-
-//   const handlerClick = (event) => {
-//     setCounter(counter+1)
-//   }
-  
-//   const handlerMouseEnter = (event) => {
-//     console.log("handlerMouseEnter")
-//   }
-//   const handlerMouseLeave = (event) => {
-//     console.log("handlerMouseLeave")
-//   }
-//   return (
-//     <button 
-//       onClick={handlerClick} 
-//       onMouseEnter={handlerMouseEnter} 
-//       onMouseLeave={handlerMouseLeave}>
-        
-//       Clicked here {counter} times
-//     </button>
-//   )
-// }
-
-
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      counter: 0
-    }
-
-    this.handlerClick = this.handlerClick.bind(this)
+const App = (props) => {
+  const handlerOnThirdClick = () => {
+      console.log('3d click here ')
   }
-  
-
-  handlerClick() {
-    this.setState({counter: this.state.counter + 1})
-  }
-  
-
-  render() {
-    return (
-          <button onClick={this.handlerClick} >
-                          
-            Clicked here {this.state.counter} times
-          </button>
-        )
-  }
+  return <Button onThirdClick={handlerOnThirdClick}/>
 
 }
+
 
 export default App;
