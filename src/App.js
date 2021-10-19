e419b2f06070f1cff9b26614bed7620ccd85d27f
Anastasia Skripnik <scripnik.a21@cf.dnu.dp.ua>
2021-10-20 00:59:04 +0300

    add class component for event handling

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
-import React, { useState } from "react"
+import React from "react"
 
-const App = (props) => {
+// const App = (props) => {
 
-  const [counter, setCounter] = useState(0)
+//   const [counter, setCounter] = useState(0)
 
-  const handlerClick = (event) => {
-    setCounter(counter+1)
+//   const handlerClick = (event) => {
+//     setCounter(counter+1)
+//   }
+  
+//   const handlerMouseEnter = (event) => {
+//     console.log("handlerMouseEnter")
+//   }
+//   const handlerMouseLeave = (event) => {
+//     console.log("handlerMouseLeave")
+//   }
+//   return (
+//     <button 
+//       onClick={handlerClick} 
+//       onMouseEnter={handlerMouseEnter} 
+//       onMouseLeave={handlerMouseLeave}>
+        
+//       Clicked here {counter} times
+//     </button>
+//   )
+// }
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      counter: 0
+    }
+
+    this.handlerClick = this.handlerClick.bind(this)
   }
   
-  const handlerMouseEnter = (event) => {
-    console.log("handlerMouseEnter")
-  }
-  const handlerMouseLeave = (event) => {
-    console.log("handlerMouseLeave")
-  }
-  return (
-    <button 
-      onClick={handlerClick} 
-      onMouseEnter={handlerMouseEnter} 
-      onMouseLeave={handlerMouseLeave}>
-        
-      Clicked here {counter} times
-    </button>
-  )
-}
 
+  handlerClick() {
+    this.setState({counter: this.state.counter + 1})
+  }
+  
+
+  render() {
+    return (
+          <button onClick={this.handlerClick} >
+                          
+            Clicked here {this.state.counter} times
+          </button>
+        )
+  }
+
+}
 
 export default App;
