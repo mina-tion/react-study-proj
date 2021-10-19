3bcfe16bdfece79148b9a1d09424c8ee8bb4557f
Anastasia Skripnik <scripnik.a21@cf.dnu.dp.ua>
2021-10-20 00:47:28 +0300

    add function component for event handling

--- src/App.js
+++ src/App.js
@@ -1,47 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
 
-class App extends React.Component {
-  constructor (props){
-    super(props)
+const App = (props) => {
 
-    this.intervalFlag = null
-    this.state = {
-      title: "Time: ",
-      date: new Date,
-      counter: 0
-    }
+  const [counter, setCounter] = useState(0)
+
+  const handlerClick = (event) => {
+    setCounter(counter+1)
   }
-
-  componentDidMount() {
-    this.intervalFlag = setInterval(() => {
-      this.setState((state, props) => ({
-        date: new Date,
-        counter: this.state.counter +1
-      }))
-    }, 1000)
+  
+  const handlerMouseEnter = (event) => {
+    console.log("handlerMouseEnter")
   }
-
-  componentDidUpdate() {
-    console.log("Component was updated")
+  const handlerMouseLeave = (event) => {
+    console.log("handlerMouseLeave")
   }
-
-
-  componentWillUnmount() {
-    clearInterval(this.intervalFlag)
-  }
-
-  render() {
-    return (
-      <div>
-        <p>{this.state.title}</p>
-        <p>{this.state.counter}</p>
-        <p>{ this.state.date.toLocaleTimeString() }</p>
-      </div>
-
-   
-    )
-  }
-
+  return (
+    <button 
+      onClick={handlerClick} 
+      onMouseEnter={handlerMouseEnter} 
+      onMouseLeave={handlerMouseLeave}>
+        
+      Clicked here {counter} times
+    </button>
+  )
 }
 
 
