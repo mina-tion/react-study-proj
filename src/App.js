5bbd4c888b5d52243d07bedceb7e4406ca47260e
Anastasia Skripnik <scripnik.a21@cf.dnu.dp.ua>
2021-10-20 17:26:18 +0300

    list rendering

--- src/App.js
+++ src/App.js
@@ -5,28 +5,32 @@ import RecursiveButton from "./RecursiveButton"
 
 const App = (props) => {
 
-  const [showMode, setShowMode] = useState(true)
+  const users = [
+    { id: 0, name: "dfd", surname: "fdf"},
+    { id: 1, name: "saaw", surname: "f"},
+    { id: 2, name: "ggf", surname: "d"},
+    { id: 3, name: "sfsf", surname: "fssdf"},
+    { id: 4, name: "dgh", surname: "fsdf"},
+  ]
 
-  let content = null
-
-  if (showMode) {
-    content = <Content />
-  }
-
-  const handlerOnClick = () => {
-    setShowMode(!showMode)
-  }
+  let list = users.map((user) => (<LiUser key={user.id} user={user}/>))
 
   return (
-    <div>
-      <RecursiveButton />
-      <button onClick={handlerOnClick}>Show toggle</button>
-      {content}
-    </div>
-  
-  )
+      <ul>
+        {list}
+      </ul>
+    )
 
 }
 
+const LiUser = (props) => {
+  const user = props.user
+
+  return (
+    <li>
+      {user.name} {user.surname}
+    </li>
+  )
+}
 
 export default App;
