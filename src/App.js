88ff835df3a5a4e2c2517b20217b46aeb5bd9c74
Anastasia Skripnik <scripnik.a21@cf.dnu.dp.ua>
2021-10-20 17:48:56 +0300

    input processing

--- src/App.js
+++ src/App.js
@@ -1,45 +1,35 @@
 import React, { useState } from "react"
-import Content from "./Content"
-import RecursiveButton from "./RecursiveButton"
 
 
 const App = (props) => {
 
-  const users = [
-    { id: 0, name: "dfd", surname: "fdf"},
-    { id: 1, name: "saaw", surname: "f"},
-    { id: 2, name: "ggf", surname: "d"},
-    { id: 3, name: "sfsf", surname: "fssdf"},
-    { id: 4, name: "dgh", surname: "fsdf"},
-  ]
+  const [value, setValue] = useState("hi!")
 
-  const handlerMouseEnter = (user) => {
-    console.log(`${user.name} ${user.surname}`)
-  }
-
-  let list = users.map((user, index) => {
-    const even = index % 2 ===0
-
-    let style = {}
-    if(even) {
-      style = {color: "white", background: "black"}
-    }
-
-    return(
-        <li key={user.id} style={style} onMouseEnter = {()=>handlerMouseEnter(user)}>
-          {user.name} {user.surname}
-        </li>
-      )
-  }
-)
-
-  return (
-      <ul>
-        {list}
-      </ul>
-    )
-
+  return(
+    <input value = {value} onChange={(e)=> setValue(e.target.value)}/>
+  ) 
 }
 
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       value: "Hi!"
+//     }
+//     this.handlerChange = this.handlerChange.bind(this)
+//   }
+
+//   handlerChange(e) { 
+//     this.setState({
+//       value: e.target.value
+//     })
+//   }
+
+//   render(){
+//     return(
+//       <input value = {this.state.value} onChange={(e) => this.handlerChange(e)}/>
+//     ) 
+//   }
+// }
 
 export default App;
