400a6449fdfaf85c134d812745e7d677620217c5
Anastasia Skripnik <scripnik.a21@cf.dnu.dp.ua>
2021-10-25 21:18:20 +0300

    redux, reducers, actions

--- src/index.js
+++ src/index.js
@@ -2,12 +2,41 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import React from "react";
+import {Provider} from "react-redux"
+import { createStore} from "redux";
 
 
+const defaultState = {
+  cash: 0,
+}
+
+
+const action = { 
+  type: "",
+  payload: 3
+}
+
+const reducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case "ADD_CASH":
+      return {...state, cash: state.cash + action.payload }
+    case "GET_CASH": 
+      return {...state, cash: state.cash - action.payload }
+
+    default: 
+      return state
+  }
+
+}
+
+const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+      
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
