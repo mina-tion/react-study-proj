e0ba8ef8d413064a80589965146adebb47c80fbd
Anastasia Skripnik <scripnik.a21@cf.dnu.dp.ua>
2021-10-25 23:34:03 +0300

    add action creators, combine reducer

--- src/index.js
+++ src/index.js
@@ -3,33 +3,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import React from "react";
 import {Provider} from "react-redux"
-import { createStore} from "redux";
+import {store} from "./store/index"
 
-
-const defaultState = {
-  cash: 0,
-}
-
-
-const action = { 
-  type: "",
-  payload: 3
-}
-
-const reducer = (state = defaultState, action) => {
-  switch(action.type) {
-    case "ADD_CASH":
-      return {...state, cash: state.cash + action.payload }
-    case "GET_CASH": 
-      return {...state, cash: state.cash - action.payload }
-
-    default: 
-      return state
-  }
-
-}
-
-const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
